21403c055115c08c4452eb407da0ace03bb132df
ElMala37 <thomasmallay99@gmail.com>
2024-03-13 22:52:30 +0100

    Presentation

--- src/composants/Presentation.js
+++ src/composants/Presentation.js
@@ -1,12 +1,62 @@
 import React, { useState } from "react";
 import Acceuil from "./Accueil";
+import "../styles/Presentation.css";
 
 const Presentation = () => {
   const [ouvert, setOuvert] = useState(true);
+  const [tiltX, setTiltX] = useState(0);
+  const [tiltY, setTiltY] = useState(0);
+
+  // Fonction pour gérer le mouvement de la souris
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    const imageContainer = event.target.getBoundingClientRect();
+
+    // Calculer l'angle d'inclinaison en fonction de la position de la souris
+    const mouseX = clientX - imageContainer.left - imageContainer.width / 2;
+    const mouseY = clientY - imageContainer.top - imageContainer.height / 2;
+
+    const newTiltX = (mouseY / (imageContainer.height / 2)) * 10;
+    const newTiltY = -(mouseX / (imageContainer.width / 2)) * 10;
+
+    setTiltX(newTiltX);
+    setTiltY(newTiltY);
+  };
+
+  // Réinitialiser l'inclinaison lorsque la souris quitte l'image
+  const handleMouseLeave = () => {
+    setTiltX(0);
+    setTiltY(0);
+  };
 
   return (
     <div className="Fond">
-      <Acceuil ouvert={ouvert} setOuvert={setOuvert}/>
+      <Acceuil ouvert={ouvert} setOuvert={setOuvert} />
+      <div className="Introduction">
+        <div
+          className="EspaceTexteIntroduction"
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img
+            src="images/carte_touraine_1.png"
+            alt="CarteTouraine1"
+            style={{
+              transform: `perspective(1000px) rotateX(${tiltX*2}deg) rotateY(${tiltY*2}deg)`,
+              transition: 'transform 0.5s ease'
+            }}
+            className="CarteTouraine1"
+          />
+        </div>
+        <div className="EspaceTexteIntroduction">
+          <p className="TexteIntroduction">
+            La Touraine est une région située dans le centre de la France. Cette
+            région possède sa propre langue, le tourangeau. Le tourangeau est
+            une langue latine et fait partie des langues d'oïl. Ce site est
+            dédié à son apprentissage.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
