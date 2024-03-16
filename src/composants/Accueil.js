0c86ef991560eae7a5be4930bc9c5d55a7da4ff7
ElMala37 <thomasmallay99@gmail.com>
2024-03-16 21:02:12 +0100

    Presentation

--- src/composants/Accueil.js
+++ src/composants/Accueil.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import "../styles/Accueil.css";
-import { motion } from "framer-motion";
 import { useNavigate } from "react-router";
 
 const Acceuil = ({ ouvert, setOuvert }) => {
-  const [etranger, setEtranger] = useState(false);
   const navigate = useNavigate();
 
   const OuvrirMenu = () => {
     setOuvert(!ouvert);
-    setEtranger(true);
   };
 
   const OuvrirPre = () => {
@@ -29,86 +26,49 @@ const Acceuil = ({ ouvert, setOuvert }) => {
 
   return (
     <div>
-      {ouvert ? (
-        <div>
-          <div className="EnTete">
+      <div>
+        <div className="EnTete">
+          {ouvert ? (
             <img
               src="/images/more.png"
               alt="OuvrirMenu"
               onClick={OuvrirMenu}
               className="LogoNavMenu"
             />
-            <div className="Titre">
-              <text className="Texte">LE TOURANGEAU</text>
-              <img
-                src="/images/drapeau.png"
-                alt="Drapeau"
-                className="Drapeau"
-              />
-            </div>
-          </div>
-          {etranger ? (
-            <motion.div
-              initial={{ x: 0 }}
-              animate={{ x: "-50vw" }}
-              transition={{ duration: 0.5 }}
-            >
-              <div className="NavMenu">
-                <button className="NavMenuBouton" onClick={OuvrirPre}>
-                  Présentation
-                </button>
-                <button className="NavMenuBouton" onClick={OuvrirVoc}>
-                  Vocabulaire
-                </button>
-                <button className="NavMenuBouton" onClick={OuvrirExe}>
-                  Exercices
-                </button>
-              </div>
-            </motion.div>
           ) : (
-            <div></div>
-          )}
-        </div>
-      ) : (
-        <div>
-          <div className="EnTete">
             <img
               src="/images/close.png"
-              alt="OuvrirMenu"
+              alt="FermerMenu"
               onClick={OuvrirMenu}
               className="LogoNavMenu"
             />
-            <div className="Titre">
-              <text className="Texte">LE TOURANGEAU</text>
-              <img
-                src="/images/drapeau.png"
-                alt="Drapeau"
-                className="Drapeau"
-              />
-            </div>
+          )}
+
+          <div className="Titre">
+            <text className="Texte">LE TOURANGEAU</text>
+            <img src="/images/drapeau.png" alt="Drapeau" className="Drapeau" />
           </div>
-          <motion.div
-            initial={{ x: "-50vw" }}
-            animate={{ x: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <div className="NavMenu">
-              <button className="NavMenuBouton" onClick={OuvrirPre}>
-                Présentation
-              </button>
-              <button className="NavMenuBouton">
-                Prononciation
-              </button>
-              <button className="NavMenuBouton" onClick={OuvrirVoc}>
-                Vocabulaire
-              </button>
-              <button className="NavMenuBouton" onClick={OuvrirExe}>
-                Exercices
-              </button>
-            </div>
-          </motion.div>
         </div>
-      )}
+
+        <div
+          className="NavMenu"
+          style={{
+            transform: ouvert ? "translateX(calc(-30vw))" : "none",
+            transition: "transform 0.5s ease",
+          }}
+        >
+          <button className="NavMenuBouton" onClick={OuvrirPre}>
+            Présentation
+          </button>
+          <button className="NavMenuBouton">Prononciation</button>
+          <button className="NavMenuBouton" onClick={OuvrirVoc}>
+            Vocabulaire
+          </button>
+          <button className="NavMenuBouton" onClick={OuvrirExe}>
+            Exercices
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
